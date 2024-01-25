d3e4c862ba6b7ef83305864a1accd840556a17d2
lenilol <emil.hillebrand@gmx.de>
2024-01-25 10:23:16 +0100

    mcd clean up

--- MonteCarloDropout/mcd.py
+++ MonteCarloDropout/mcd.py
@@ -7,7 +7,6 @@ import random
 #main methode all others functions are called from there
 def get_mcd_uncertainty(image, model, preprocess, decode, samples, dropoutRate, applyOrSkip, apply_skip_list):
     #load picture
-    #picture_input = np.expand_dims(image, axis=0)
     picture_input_preprocessed = preprocess(image[None])  
     
     #models
@@ -17,25 +16,22 @@ def get_mcd_uncertainty(image, model, preprocess, decode, samples, dropoutRate, 
     #list of decoded predictions
     predictions_readable = []
 
+    skip_apply_list = []
+
     #creating List once so not done in every iteration
     if (applyOrSkip == "Skip these layers"):
-        skipList = skipLayers(apply_skip_list, modelMC)
+        skip_apply_list = skipLayers(apply_skip_list, modelMC)
     else:
-        applytoList = applyTo(apply_skip_list, modelMC)
+        skip_apply_list = applyTo(apply_skip_list, modelMC)
 
     #apply MonteCarlo and write predictions
     for r in range(samples):    
         #skip or apply to these layers
-        if (applyOrSkip == "Skip these layers"):
-        #applyTo or not apply to
-            modelMC = applyMonteCarloSkip(skipList, modelMC, modelNoMC, dropoutRate)
-        else:
-            modelMC = applyMonteCarloApplyTo(applytoList, modelMC, modelNoMC, dropoutRate)
-            
+        modelMC = applyMonteCarloApplyTo(skip_apply_list, modelMC, modelNoMC, dropoutRate)
         predictions = modelMC.predict(picture_input_preprocessed)
         predictions_readable.append(decode(predictions))
     
-    #mean of predictions    
+    #prediction    
     uncertainty = processPredictions(predictions_readable, modelNoMC, picture_input_preprocessed, decode)
 
     return uncertainty
@@ -112,55 +108,6 @@ def applyMonteCarloApplyTo(applytoList, modelMC, modelNoMC, dropoutRate):
                 modelMC.layers[i].set_weights(b)
     return modelMC
 
-def applyMonteCarloSkip(skipList, modelMC, modelNoMC, dropoutRate = 20, loopyloops = 10):
-    for i in range(len(modelMC.layers)):
-        if((i) not in skipList):
-            b = []
-            b = modelNoMC.layers[i].get_weights()
-            for q in range(len(b)):
-                if(str(type(b[q])) == "<class 'numpy.float32'>"):
-                    if(random.randint(1, 100) <= dropoutRate):
-                        #print("1. change to zero")
-                        b[q] = 0.00000000
-                else:
-                    for j in range(len(b[q])):
-                        if(str(type(b[q][j])) == "<class 'numpy.float32'>"):
-                            if(random.randint(1, 100) <= dropoutRate):
-                                #print("2. change to zero")
-                                b[q][j] = 0.00000000
-                        else:
-                            for k in range(len(b[q][j])):
-                                if(str(type(b[q][j][k])) == "<class 'numpy.float32'>"):
-                                    if(random.randint(1, 100) <= dropoutRate):
-                                        #print("3. change to zero")
-                                        b[q][j][k] = 0.00000000
-                                else:
-                                    for l in range(len(b[q][j][k])):
-                                        if(str(type(b[q][j][k][l])) == "<class 'numpy.float32'>"):
-                                            if(random.randint(1, 100) <= dropoutRate):
-                                                #print("4. change to zero")
-                                                b[q][j][k][l] = 0.00000000
-                                        else:        
-                                            for m in range(len(b[q][j][k][l])):
-                                                if(str(type(b[q][j][k][l][m])) == "<class 'numpy.float32'>"):
-                                                    if(random.randint(1, 100) <= dropoutRate):
-                                                        #print("5. change to zero")
-                                                        b[q][j][k][l][m] = 0.00000000
-                                                else:
-                                                    for n in range(len(b[q][j][k][l][m])):
-                                                        if(str(type(b[q][j][k][l][m][n])) == "<class 'numpy.float32'>"):
-                                                            if(random.randint(1, 100) <= dropoutRate):
-                                                                #print("6. change to zero")
-                                                                b[q][j][k][l][m][n] = 0.00000000
-                                                        else:
-                                                            for o in range(len(b[q][j][k][l][m][n])):
-                                                                if(str(type(b[q][j][k][l][m][n][o])) == "<class 'numpy.float32'>"):
-                                                                    if(random.randint(1, 100) <= dropoutRate):
-                                                                        #print("7. change to zero")
-                                                                        b[q][j][k][l][m][n][o] = 0.00000000
-                modelMC.layers[i].set_weights(b)
-    return modelMC
-
 def processPredictions(predictions_readable, modelNoMC, picture_input_preprocessed, decode):
     counter = 0
     predictionNormalModel = decode(modelNoMC.predict(picture_input_preprocessed))
@@ -170,5 +117,5 @@ def processPredictions(predictions_readable, modelNoMC, picture_input_preprocess
             counter = counter + 1
 
     #print (predictionNormalModel)
-    mean = str(counter/len(predictions_readable))
-    return mean
+    hits = str(counter/len(predictions_readable))
+    return hits
