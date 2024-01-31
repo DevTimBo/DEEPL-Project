3c6308af7afef07d6be16e8a8b1c5a56a48b18b1
Jason Pranata <83290503+sonotokay@users.noreply.github.com>
2024-01-31 01:19:42 +0100

    added some funtions cause filepaths are hard coded and i dont wanna ruin hadis stuff

--- framework_grad_video.py
+++ framework_grad_video.py
@@ -1,6 +1,11 @@
-from CAM import video, grad_cam
+from CAM import grad_cam
+import framework_video as video
 import cv2
 import os 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import keras
 
 def make_gradcam_video(model, video_path_in, img_size, preprocess_input, decode_predictions, last_conv_layer_name):
     capture = cv2.VideoCapture(video_path_in)
@@ -16,7 +21,7 @@ def make_gradcam_video(model, video_path_in, img_size, preprocess_input, decode_
     i = 0
     for image in sorted_frames:
         img_path = os.path.join(FRAME_FOLDER, image)
-        grad_cam.make_gradcam(model, img_path, img_size, 
+        make_gradcam(model, img_path, img_size, 
                             preprocess_input, decode_predictions, last_conv_layer_name, i)
         preds.append(grad_cam.get_pred())
         print(preds)
@@ -54,13 +59,71 @@ def extract_number(filename):
     filename = filename.split('.jpg')[0]
     return int(filename)
 
+OUTPUT_FOLDER = 'data\gradcam_output'
+OUTPUT_FOLDER_SH = r'data\gradcam_output\Small_Heatmap'
+OUTPUT_FOLDER_MH = r'data\gradcam_output\Mid_Heatmap'
+OUTPUT_FOLDER_LH = r'data\gradcam_output\Large_Heatmap'
+
+def make_gradcam(model, img_path, img_size, preprocess, decode_predictions, last_conv_layer_name, frameNr = ''):
+    global prediction, heatmap_name2
+    heatmap_name = f'cam1_1{frameNr}.jpg'
+    heatmap_name2 = f'cam1_2{frameNr}.jpg'
+    result_name = f'cam1_3{frameNr}.jpg'
+    preprocess_input = preprocess
+    img_array = preprocess_input(grad_cam.get_img_array(img_path, size=img_size))
+    # print(img_array)
+    model.layers[-1].activation = None  # OPTIONAL
+    preds = model.predict(img_array)
+    prediction = decode_predictions(preds, top=1)[0]
+    print("Predicted:", prediction)
+    heatmap = grad_cam.make_gradcam_heatmap(img_array, model, last_conv_layer_name)
+    plt.imshow(heatmap)
+    plt.savefig(os.path.join(OUTPUT_FOLDER_SH, heatmap_name))
+    result_path = os.path.join(OUTPUT_FOLDER_LH, result_name)
+    save_and_display_gradcam(img_path, preds, heatmap, result_path, alpha=0.4)
+    
+def save_and_display_gradcam(img_path, preds, heatmap, cam_path, alpha=0.4):
+    global superimposed_img, test_superim
+
+    img = keras.utils.load_img(img_path)
+    img = keras.utils.img_to_array(img)
+
+    heatmap = np.uint8(255 * heatmap)
+
+    jet = mpl.colormaps["jet"]
+
+    jet_colors = jet(np.arange(256))[:, :3]
+    jet_heatmap = jet_colors[heatmap]
+
+    jet_heatmap = keras.utils.array_to_img(jet_heatmap)
+    jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+    jet_heatmap = keras.utils.img_to_array(jet_heatmap)
+
+    superimposed_img = jet_heatmap * alpha
+    superimposed_img = keras.utils.array_to_img(superimposed_img)
+    mid_path = os.path.join(OUTPUT_FOLDER_MH, heatmap_name2)
+    superimposed_img.save(mid_path)
+
+    superimposed_img = jet_heatmap * alpha + img
+    superimposed_img = keras.utils.array_to_img(superimposed_img)
+
+    superimposed_img.save(cam_path)
+
+    # display(Image(cam_path)) --> Original
+    # plt.imshow(superimposed_img)
+    # plt.colorbar()
+    # plt.title(decode_predictions(preds, top=1)[0])
+
+def get_pred():
+    return prediction
+
 if __name__ == "__main__":
     import sys
     from custom import custom_model
     
-    FRAME_FOLDER = r'CAM\video_Frames'
-    LH_frames = r'CAM\Images\gradcam_output\Large_Heatmap'
-    SH_frames = r'CAM\Images\gradcam_output\Small_Heatmap'
+    FRAME_FOLDER = r'data\frames'
+    LH_frames = r'data\gradcam_output\Large_Heatmap'
+    SH_frames = r'data\gradcam_output\Small_Heatmap'
     video_out_LH = r'data\LH_video.avi'
     video_out_SH = r'data\SH_video.avi' 
     fps = 24
@@ -79,9 +142,11 @@ if __name__ == "__main__":
 
     if model_name == "VGG16":
         import keras.applications.vgg16 as vgg16
-
         # Keras Model
         model = vgg16.VGG16(weights="imagenet")
+        img_size = (224, 224)
+        preprocess = vgg16.preprocess_input
+        decode_predictions = vgg16.decode_predictions
     else:
 
         custom_model_mapping_path = sys.argv[7]
@@ -101,8 +166,8 @@ if __name__ == "__main__":
             if 'conv' in layer.name:
                 last_conv_layer = layer.name
                 break
-    preprocess = custom_model.preprocess
-    decode_predictions = custom_model.decode_predictions
+        preprocess = custom_model.preprocess
+        decode_predictions = custom_model.decode_predictions
 
     # Call your function
     make_gradcam_video(model, filepath, img_size, preprocess,
