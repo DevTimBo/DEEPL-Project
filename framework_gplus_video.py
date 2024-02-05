e2041093fdcbba2a7fdc7c24ff44728749d9daff
Jason Pranata <83290503+sonotokay@users.noreply.github.com>
2024-02-05 17:09:28 +0100

    made framework gcam plus video

---
+++ framework_gplus_video.py
@@ -0,0 +1,108 @@
+import framework_video as video
+import framework_gcam_plus as gplus
+import cv2
+import os 
+import keras 
+
+FRAME_FOLDER = r'data\frames'
+LH_frames = r'data\gradcam_output\Large_Heatmap'
+SH_frames = r'data\gradcam_output\Small_Heatmap'
+video_out_LH = r'data\LH_video.avi'
+video_out_SH = r'data\SH_video.avi' 
+fps = 24
+
+OUTPUT_FOLDER = 'data\gcam_plus_output'
+heatmap_name = 'cam1_1.jpg'
+
+def make_gradcamplusplus_video(model, video_path_in, target_size, last_conv_layer_name):
+    capture = cv2.VideoCapture(video_path_in)
+    video.cut_video(capture)
+    sorted_frames = sorted(os.listdir(FRAME_FOLDER), key=extract_number)
+    for img in sorted_frames:
+        print(img)
+
+    preds = []
+
+    # Wendet gradcam++ auf jedes Frame an 
+    i = 0
+    for image in sorted_frames:
+        img_path = os.path.join(FRAME_FOLDER, image)
+        gplus.make_gradcam_plusplus(model, img_path, last_conv_layer_name, target_size)
+        #preds.append(grad_cam.get_pred())
+        #print(preds)
+        i += 1 
+        if i == 50:
+            break
+
+    sorted_frames_LH = sorted(os.listdir(LH_frames), key=video.extract_number)
+    sorted_frames_SH = sorted(os.listdir(SH_frames), key=video.extract_number)
+
+def extract_number(filename):
+    filename = filename.split('.jpg')[0]
+    return int(filename)
+
+
+if __name__ == "__main__":
+    import sys
+    from custom import custom_model
+    
+    FRAME_FOLDER = r'data\frames'
+    LH_frames = r'data\gradcam_output\Large_Heatmap'
+    SH_frames = r'data\gradcam_output\Small_Heatmap'
+    video_out_LH = r'data\LH_video.avi'
+    video_out_SH = r'data\SH_video.avi' 
+    fps = 24
+    
+    # Extract command-line arguments
+    model_name = sys.argv[1]
+    filepath = sys.argv[2]
+    json_string = sys.argv[3]
+    import json
+    
+    img_size = json.loads(json_string)
+    custom_model_path = sys.argv[4]
+    custom_model_weights_path = sys.argv[5]
+    print(f"Model :{model_name}:")
+
+    if model_name.strip() == "VGG16":
+        import keras.applications.vgg16 as vgg16
+
+        # Keras Model
+        model = vgg16.VGG16(weights="imagenet")
+        img_size = (224, 224)
+        preprocess = vgg16.preprocess_input
+        decode_predictions = vgg16.decode_predictions
+    elif model_name.strip() == "VGG19":
+        import keras.applications.vgg19 as VGG19
+
+        # Keras Model
+        model = VGG19.VGG19(weights="imagenet")
+        img_size = (224, 224)
+        preprocess = VGG19.preprocess_input
+        decode_predictions = VGG19.decode_predictions
+
+    else:
+
+        custom_model_mapping_path = sys.argv[6]
+        custom_model.set_csv_file_path(custom_model_mapping_path)
+        custom_model.set_size(img_size)
+        channel_num = sys.argv[7]
+        custom_model.set_channels(int(channel_num))
+
+        import keras
+
+        model = keras.models.load_model(custom_model_path)
+        model.load_weights(custom_model_weights_path)
+
+        all_layers = model.layers
+        last_conv_layer = None
+        for layer in reversed(all_layers):
+            if 'conv' in layer.name:
+                last_conv_layer = layer.name
+                break
+        preprocess = custom_model.preprocess
+        decode_predictions = custom_model.decode_predictions
+
+    # Call your function
+    make_gradcamplusplus_video(model, filepath, img_size, last_conv_layer)
+    
